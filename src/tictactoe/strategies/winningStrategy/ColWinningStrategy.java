fc27faa9080d1e326a700071be1b1f10d9c8c1f6
Kiran777-jpg <suryakirananusuri@gmail.com>
2026-01-03 23:04:47 +0530

    Implemented Col and Diagonal Winning Strategies

--- src/tictactoe/strategies/winningStrategy/ColWinningStrategy.java
+++ src/tictactoe/strategies/winningStrategy/ColWinningStrategy.java
@@ -3,9 +3,19 @@ package tictactoe.strategies.winningStrategy;
 import tictactoe.models.Board;
 import tictactoe.models.Move;
 
+import java.util.HashMap;
+
 public class ColWinningStrategy implements WinningStrategy{
+
+    private final HashMap<Integer, HashMap<Character, Integer>> colMaps = new HashMap<>();
     @Override
     public boolean checkWinner(Board board, Move move) {
-        return false;
+        int col = move.getCell().getCol();
+        Character aChar = move.getPlayer().getSymbol().getaChar();
+
+        colMaps.putIfAbsent(col, new HashMap<>());
+        HashMap<Character, Integer> currColMap = colMaps.get(col);
+        currColMap.put(aChar, currColMap.getOrDefault(aChar, 0) + 1);
+        return currColMap.get(aChar).equals(board.getDimensions());
     }
 }
--- src/tictactoe/strategies/winningStrategy/DiagonalWinningStrategy.java
+++ src/tictactoe/strategies/winningStrategy/DiagonalWinningStrategy.java
@@ -3,9 +3,29 @@ package tictactoe.strategies.winningStrategy;
 import tictactoe.models.Board;
 import tictactoe.models.Move;
 
+import java.util.HashMap;
+
 public class DiagonalWinningStrategy implements WinningStrategy{
     @Override
     public boolean checkWinner(Board board, Move move) {
+        HashMap<Character, Integer> leftDiagonalMap = new HashMap<>();
+        HashMap<Character, Integer> rightDiagonalMap = new HashMap<>();
+
+        int row = move.getCell().getRow();
+        int col = move.getCell().getCol();
+        Character aChar = move.getPlayer().getSymbol().getaChar();
+
+        if(row == col) {
+            leftDiagonalMap.put(aChar, leftDiagonalMap.getOrDefault(aChar, 0) + 1);
+            if(leftDiagonalMap.get(aChar).equals(board.getDimensions())) {
+                return true;
+            }
+        }
+
+        if(row + col == board.getDimensions()-1) {
+            rightDiagonalMap.put(aChar, rightDiagonalMap.getOrDefault(aChar, 0) + 1);
+            return rightDiagonalMap.get(aChar).equals(board.getDimensions());
+        }
         return false;
     }
 }
